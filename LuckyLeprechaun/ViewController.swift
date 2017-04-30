ed859831e473f5f4cb3a8c99cb81497edea72afa
fantomcadet <deedee.rene.rich@hotmail.com>
2017-04-30 14:21:55 -0600

    First commit for Lucky Leprechaun

--- LuckyLeprechaun/ViewController.swift
+++ LuckyLeprechaun/ViewController.swift
@@ -5,6 +5,14 @@
 //  Created by Dee Dee Rich on 4/30/17.
 //  Copyright © 2017 Dee Dee Rich. All rights reserved.
 //
+//Lucky Leprechaun is a Pokemon Go like game in which the player navigates in game and has
+//random encounters with a few monsters and a couple items
+
+//1st iteration - loads one of type monster or treasure randomly into specified GPS locations
+//Requires player class, monsters (2-4 monsters), items (2-4 objects), mobility controls (walking), touch controls (attack/interact), label to display gold/points
+//2nd iteration - inventory and store
+//3rd iteration - attempt to access camera and use for augmented reality
+//4th iteration - ensure that game doesn't work when at a certain velocity
 
 import UIKit
 
---
+++ README.md
@@ -0,0 +1,7 @@
+### Lucky Leprechaun
+
+##### Made by Dee Dee Rich
+
+This is a Pokemon Go like game where a leprechaun or other Irish/Scottish
+baddie or treasure is randomly instantiated in a GPS location.  The
+player is required to tap the item(s) to interact.
