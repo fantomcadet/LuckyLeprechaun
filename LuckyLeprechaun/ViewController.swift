43b027b1ee4e2f911f52cbfe1b4f9109086133ac
fantomcadet <deedee.rene.rich@hotmail.com>
2017-04-30 17:04:52 -0600

    Initial Commit

---
+++ LuckyLeprechaun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuckyLeprechaun
+//
+//  Created by Dee Dee Rich on 4/30/17.
+//  Copyright © 2017 Dee Dee Rich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
